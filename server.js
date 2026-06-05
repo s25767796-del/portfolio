1fe35c88e7de2016708a3515ceb656e0976e7f44
s25767796-del <s25767796@gmail.com>
2026-06-05 13:51:53 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,9 @@
+// smooth scroll
+document.querySelectorAll('a[href^="#"]').forEach(a => {
+  a.addEventListener("click", e => {
+    e.preventDefault();
+    document.querySelector(a.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
